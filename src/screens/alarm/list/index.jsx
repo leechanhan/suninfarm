b9429a98c600fb6edaa791518e54066c74c8b914
leechanhan <leechanhan@gmail.com>
2024-07-26 00:03:40 +0900

    ㅁㄴㅇ

--- src/screens/alarm/list/index.jsx
+++ src/screens/alarm/list/index.jsx
@@ -4,7 +4,6 @@ import { useEffect, useState } from 'react';
 import CustomForms from '@component/etc/CustomForms';
 import { openPage } from '@lib/hooks/common';
 import Button뒤로가기 from '@component/frame/headerBtn/Button뒤로가기';
-import Button자유함수 from '@component/frame/headerBtn/Button자유함수';
 const AlarmScreen = () => {
 	const router = useRouter();
 
@@ -17,12 +16,6 @@ AlarmScreen.Layout = Layout기본헤더;
 AlarmScreen.headerOptions = {
 	title: '알람',
 	LeftButton: () => <Button뒤로가기 />,
-	RightButton: () => (
-		<Button자유함수
-			routerPath={'/alarm/add'}
-			imgPath={'/images/png/button_plus.png'}
-		/>
-	),
 };
 
 AlarmScreen.isGnb = false;
